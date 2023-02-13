7851f586b57c3ac684b4c216aa2a0acc2ba53cec
MARTIN HANSEN <marrtt1234@gmail.com>
2023-02-13 20:33:46 +0000

    test aint working

---
+++ handlers/500.js
@@ -0,0 +1,12 @@
+//500 is server error
+'use strict';
+
+module.exports = (err, req, res, next) => {
+  res.status(500).send({
+    error: 500,
+    route: req.path,
+    query : req.query,
+    body: req.body,
+    message: `Server Error: ${err}`,
+  });
+};
